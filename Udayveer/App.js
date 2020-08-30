56974744f53779035f09aab8a9772c2c6f254b3f
udayveer16 <udayveer1609@gmail.com>
2020-08-30 21:19:37 +0530

    Product display page

---
+++ Udayveer/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Image from './component/Image'
+import ShopHeader from './component/Shop_Header'
+import './Style/Shop_Header.css'
+import ProductName from './component/Product_Name'
+import ProductDetail from './component/Product_Discription'
+import ShopDetail from './component/Shop_detail'
+
+
+function App() {
+  return (
+    <div className="App">
+      <ShopHeader name="" />
+      <Image />
+      <ProductName />
+      <ProductDetail />
+      <ShopDetail />
+    </div>
+  );
+}
+
+export default App;
