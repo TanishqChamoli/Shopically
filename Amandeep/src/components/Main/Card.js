656a446e21e5c988f9ba64f6b05112b6e3e9364a
Udayveer Singh <42859352+udayveer16@users.noreply.github.com>
2020-09-01 15:51:27 +0530

    Update Card.js

--- Amandeep/src/components/Main/Card.js
+++ Amandeep/src/components/Main/Card.js
@@ -1,3 +1,8 @@
+/*This is the card using which product will be shown
+Pass parameter product id to this function and retrieve the information using this id from the database
+If you want you can change this simple function into class
+This card will be used in home page, search page and shop display page*/
+
 import React from 'react';
 import './Main.css'
 const card=()=>{
