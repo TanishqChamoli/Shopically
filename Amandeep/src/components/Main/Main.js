13cc85b9f48df2d75fe07857fa685922fe77f449
Udayveer Singh <42859352+udayveer16@users.noreply.github.com>
2020-09-01 16:11:47 +0530

    Update Main.js

--- Amandeep/src/components/Main/Main.js
+++ Amandeep/src/components/Main/Main.js
@@ -1,3 +1,12 @@
+/*This is the search page main component
+when product button is clicked then the cards should show products
+and if shop button is clicked the shop info should be retrieved from the database.
+Try to apply the infinite loop of product retrieval as discussed
+side filter has been discussed in side bar component.
+Hint:- you can retrieve the information in this file for the card and apply a loop in which different productID are passed it as attribute in the card tag
+*/
+
+
 import React,{Fragment} from 'react';
 import './Main.css';
 import Card from './Card';
@@ -7,11 +16,11 @@ import ProductButton from './Product_button';
 const main=()=>{
     return (
         <div className="d-flex">
-            <Sidebar />
+            <Sidebar />// the side filter component
             <div className="main-div">
                 <div className="d-flex">
-                    <ProductButton /> 
-                    <ShopButton />
+                    <ProductButton /> //the product button
+                    <ShopButton /> //the shop button
                 </div>
                 <div className="d-flex">
                     <Card />
