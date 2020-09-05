5660eb2a70fc852201f6501e6b44ff39dd248bb4
Tanveer Singh <42859006+CO18353@users.noreply.github.com>
2020-09-05 23:22:31 +0530

    To display data from db for homepage

---
+++ Tanveer/backend/homepagedata.php
@@ -0,0 +1,26 @@
+<?php
+    $host = '127.0.0.1';
+    $db   = 'shopically';
+    $user = 'root';
+    $pass = '';
+
+    $dsn = "mysql:host=$host;dbname=$db";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        /*$recvText = $_POST['text'];
+        $sql = "INSERT INTO react_php (id,texts) VALUES(1,?)";
+        $pdo->prepare($sql)->execute([$recvText]);*/
+        $sql = "SELECT `product_id`, `product_name`, `product_brand`, `product_price`,`product_image` FROM `product` WHERE location='chd'";
+        $res=$pdo->prepare($sql);
+        $res->execute();
+        $ans=$res->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($ans);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+?>
