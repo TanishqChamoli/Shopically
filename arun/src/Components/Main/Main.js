075f859b85da3e89e0c774f5461de2187793e7ac
Udayveer Singh <42859352+udayveer16@users.noreply.github.com>
2020-09-05 23:37:05 +0530

    Update Main.js
    Comments added

--- arun/src/Components/Main/Main.js
+++ arun/src/Components/Main/Main.js
@@ -1,3 +1,9 @@
+/*this is a shop display page
+make this function a class and pass the shop id in the constructor and retrieve the products of this shop on this page and show them using cards
+Reminder-cards of this directory will not be used
+filter menu of this shop will contain only those thags that are available in the shop
+infinite loop will be used here too*/
+
 import React from 'react';
 import './Main.css'
 import Card from './Card';
