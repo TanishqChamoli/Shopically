6a2452adc5a3a5386b58d99b15eb759ddedcd832
Tanishq Chamoli <tanishq.chamoli@gmail.com>
2020-09-07 22:44:54 +0530

    Updated the add product anf added src folder.

---
+++ Sonam/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import "./components/signup.js";
+// import Signup from "./signup";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<Signup />, document.getElementById("signup"));
