373745ffb69b2bee9315d6df3f3058cce23d89e2
Tanveer Singh <germinostiltion@gmail.com>
2021-01-06 11:21:49 +0530

    Built

---
+++ build/precache-manifest.3b6d48aea8f28a22c5b645bcedb33452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e7e42f44693ccbe91c2a27f6da0f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a538c59c8d9bea963d8f",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "f369675ebf5055be765e",
+    "url": "/static/css/main.d7a21ab5.chunk.css"
+  },
+  {
+    "revision": "a538c59c8d9bea963d8f",
+    "url": "/static/js/2.7d8bdd15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7d8bdd15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f369675ebf5055be765e",
+    "url": "/static/js/main.a3fe9f6b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
